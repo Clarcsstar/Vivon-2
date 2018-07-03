c784756e5f8e17cd576b24077ea275f45514c61b
SAnatoly <Anatoly.Shapov@yandex.ru>
2018-07-03 13:53:34 +0300

    Communism for you

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 20f; // Общедоступная переменная скорости 
+    public float rotationSpeed = 10;
+    public float movingDistance = 8; //До какого расстояния дойдет обьект по X
+    public bool randomSpeed;
+    public float maxSpeed = 16;
+	void Start ()
+    {
+       if (randomSpeed)
+        {
+            speed = Random.Range(1, 16);
+        }
+	}
+
+	void Update ()
+    {
+        // Если положение обьекта больше равен MovingDistance поворачиваем
+        if (transform.position.x >= movingDistance || transform.position.x <= -movingDistance)   
+        {
+            speed = -speed;
+        }
+        
+
+        transform.Translate(speed * Time.deltaTime, 0, 0); // Передвигает обьект
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0); //Вращает обьект
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 6; // скорость игрока
+    public float jumpSpeed = 10; ///скорость прыжка
+    public float rotationSpeed = 10;
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Move();
+	}
+    void Move()
+    {
+        //Получаем значение осей от 1 до - 1
+        float moveHorizontal = Input.GetAxis("Horizontal")* speed * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveUp = 0f;
+        float rotateHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // при нажатии клавиши прыжка меняем значение moveUp на скорость прыжка
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveUp = jumpSpeed * Time.deltaTime;
+        }
+        transform.Rotate(0, rotateHorizontal, 0);
+        transform. Translate(moveHorizontal, moveUp, moveVertical); // перемещение
+    }
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
